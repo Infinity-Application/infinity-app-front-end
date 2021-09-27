d9d3e0a96657c7175875d60776f7f7409cbed70f
Leen Hazaimeh <leenhazaimeh94@gmail.coom>
2021-09-27 20:25:02 +0300

    add the setting func

--- screens/Profile.js
+++ screens/Profile.js
@@ -2,26 +2,82 @@ import React from 'react';
 import {
     View,
     Text,
-    TouchableOpacity;
+    TouchableOpacity,
     ScrollView,
     Image,
-    Switch, 
-    View
+    Switch
+   
 } from 'react-native';
-// import { FONTS, COLORS, SIZE, dummyData, icons } from "../constants";
 
-const Profile = () => {
+import { MainLayout } from "./";
+import { HeaderBar } from "../components";
+import { FONTS, COLORS, SIZE, dummyData, icons } from "../constants";
+
+
+
+const SectionTitle = ({ title }) =>  {
     return (
-        <View>
-            <Text>Profile</Text>
+        <View
+            style={{
+                marginTop: SIZES.padding
+            }}
+        >
+            <Text style={{ color: COLORS.lightGray3, ...FONTS.h4 }}>{title}</Text>
         </View>
     )
 }
-const SectionTitle = {{title}} => {
-    return{ <View></View>
+
+const Setting = ({ title, value, type, onPress }) => {
+    if (type == "button") {
+        return (
+            <TouchableOpacity
+                style={{
+                    flexDirection: 'row',
+                    height: 50,
+                    alignItems: 'center'
+                }}
+                onPress={onPress}
+            >
+                <Text style={{ flex: 1, color: COLORS.white, ...FONTS.h3 }}>{title}</Text>
+
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center'
+                    }}
+                >
+                    <Text style={{ marginRight: SIZES.radius, color: COLORS.lightGray3, ...FONTS.h3 }}>{value}</Text>
+                    <Image
+                        source={icons.rightArrow}
+                        style={{
+                            height: 15,
+                            width: 15,
+                            tintColor: COLORS.white
+                        }}
+                    />
+                </View>
+            </TouchableOpacity>
+        )
+    } else {
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    height: 50,
+                    alignItems: 'center'
+                }}
+            >
+                <Text style={{ flex: 1, color: COLORS.white, ...FONTS.h3 }}>{title}</Text>
+                <Switch
+                    value={value}
+                    onValueChange={(value) => onPress(value)}
+                />
+            </View>
+        )
+    }
 
 }
-const Profile = {} => {
+const Profile = () => {
     const [faceId, setfaceId] =  React.useState(true)
     return {
         <MainLayout>
