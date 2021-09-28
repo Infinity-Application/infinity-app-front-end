ae197548c5e3462ab433729f6f89687c8b20ffcb
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-09-28 21:41:19 +0300

    feat: working on index.js importing completed components

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
+import Home from "./Home"
+import Portfolio from "./Portfolio"
+import Profile from "./Profile"
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export {
+    Home,
+    Portfolio,
+    Profile
+}
